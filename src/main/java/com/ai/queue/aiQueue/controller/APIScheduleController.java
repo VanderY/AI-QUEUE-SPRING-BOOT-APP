f7c81799383e1452021a30d0e726848dba529f4c
Vander <vander706@gmail.com>
2021-09-30 03:00:18 +0300

    restored schedule api

--- src/main/java/com/ai/queue/aiQueue/controller/APIScheduleController.java
+++ src/main/java/com/ai/queue/aiQueue/controller/APIScheduleController.java
@@ -1,5 +1,6 @@
 package com.ai.queue.aiQueue.controller;
 
+import com.ai.queue.aiQueue.POJO.Schedule;
 import com.ai.queue.aiQueue.POJO.Student;
 import com.ai.queue.aiQueue.POJO.StudentQueue;
 import com.ai.queue.aiQueue.entity.StudentEntity;
@@ -12,10 +13,32 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class APIScheduleController {
+
+    private ScheduleServiceImpl scheduleServiceImpl;
+
+    @Autowired
+    @Qualifier("scheduleServiceImpl")
+    public void setScheduleServiceImpl(ScheduleServiceImpl scheduleServiceImpl) {
+        this.scheduleServiceImpl = scheduleServiceImpl;
+    }
+
+    @GetMapping(value = {"/schedule/{group}","/schedule/{group}/{date}"})
+    public List<Schedule> getSchedule(@PathVariable String group, @PathVariable(required = false) Date date){
+        if(date != null) {
+            return scheduleServiceImpl.getScheduleListByGroupAndDate(group, date);
+            //return scheduleServiceImpl.getScheduleListByGroupAndDate("921703", Date.valueOf("2021-09-27"));
+        }
+        else{
+            return  scheduleServiceImpl.getScheduleListByGroup(group);
+        }
+    }
 
     private StudentQueueServiceImpl studentQueueService;
     private StudentServiceImpl studentService;
